b67d6b73680dc920e1ef8a43ba7c4889271c91d2
Ibrahim <ibrahim.abdou2121@gmail.com>
2024-06-11 19:01:11 +0200

    LRU properly implemented and HITS are more visible in output.

--- LRU,OPT,FIFO.c
+++ LRU,OPT,FIFO.c
@@ -10,7 +10,7 @@ int hits = 0;
 
 int main(){
     //char str[100];
-    char str[] = "12342156212376321236";
+    char str[] = "70120304230321201701";
     //char choice[5];
     char choice[] = "FIFO";
     int size = 0;
@@ -49,52 +49,55 @@ bool isHit(char page, char * frames[], int frameCount){
 
 void LRU(char * refString, int maxFrames){
 
-    //PROPERLY IMPLEMENT LRU
-
     char * memoryPtr[maxFrames];
-    // Initialize memory frames to NULL.
     for (int i = 0; i < maxFrames; i++) {
         memoryPtr[i] = NULL;
     }
-    int counter = 0;
 
-
-    char * leastRU[maxFrames];
+    char * hitDetection[maxFrames];                       // Array to store hit bits.
     for (int i = 0; i < maxFrames; i++) {
-        leastRU[i] = NULL;
+        hitDetection[i] = 0;
     }
 
-    for (int i=0; i < strlen(refString);++i){
-        if(isHit(refString[i], memoryPtr, maxFrames) == false){     // On page fault.
-            if (leastRU[counter] == NULL){
-                memoryPtr[counter] = &refString[i];
-                leastRU[counter] = &refString[i];
-                pageFaults++;
+    int time = 0;                                         // Simulate time to get the LRU index.
+
+    for (int i = 0; i < strlen(refString); ++i) {
+        time++;
+        char currentPage = refString[i];                                // Get current page ref.
+
+        if (!isHit(currentPage, memoryPtr, maxFrames)) {                // On page fault.
+            int lruIndex = 0;
+            for (int j = 1; j < maxFrames; j++) {
+                if (hitDetection[j] < hitDetection[lruIndex]) {         // Check for the shortest time and get the matching index.
+                    lruIndex = j;
+                }
             }
-            else{
-                // Implement it here
+
+            memoryPtr[lruIndex] = &refString[i];                        // Store address of ref in refstring in memoryPtr with index lruIndex.
+            hitDetection[lruIndex] = time;                              // Assign new time to index.
+            pageFaults++;
+
+        } else {
+            printf("----HIT----\n");                                    // On hit
+            for (int t = 0; t < maxFrames; t++) {
+                if (memoryPtr[t] != NULL && *memoryPtr[t] == currentPage) {
+                    hitDetection[t] = time;
+                    break;
+                }
             }
-
-
-
-        }
-        else {                                                      // On hit.
             hits++;
         }
 
-        counter++;
-        if (counter == maxFrames){                                  // Reset counter.
-            counter = 0;
-        }
-
-        char result[maxFrames];                                    // Temporary array to be returned.
-        for (int i = 0; i < maxFrames; i++) {                       // Loop through frames and return their result.
-            if (memoryPtr[i] != NULL ) {
-                result[i] = *memoryPtr[i];
-                printf("%c",result[i]);
+        char result[maxFrames];                                     // Temporary array to be returned.
+        for (int c = 0; c < maxFrames; c++) {                       // Loop through frames and return their result.
+            if (memoryPtr[c] != NULL ) {
+                result[c] = *memoryPtr[c];
+                printf("%c",result[c]);
             }
+
         }
         printf("\n");
+
     }
 
 }
@@ -108,7 +111,6 @@ void FIFO(char * refString, int maxFrames){
         memoryPtr[i] = NULL;
     }
     int counter = 0;
-
     // Loop through reference string and check if a hit/page faults occurs.
     // Compute the new string that fits into the specified frame amount.
     for (int i=0; i < strlen(refString);++i){
@@ -122,6 +124,7 @@ void FIFO(char * refString, int maxFrames){
 
         }
         else {                                                      // On hit.
+            printf("----HIT----\n");
             hits++;
         }
 
