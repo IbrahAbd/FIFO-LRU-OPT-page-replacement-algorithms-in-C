d56dce33616faa50b01d55135f1c8c0f113f56ad
Ibrahim <ibrahim.abdou2121@gmail.com>
2024-06-12 11:20:20 +0200

    Added OPT algorithm and choice branches.

--- LRU,OPT,FIFO.c
+++ LRU,OPT,FIFO.c
@@ -4,33 +4,40 @@
 
 void FIFO(char * refString, int maxFrames);
 void LRU(char * refString, int maxFrames);
+void OPT(char * refString, int maxFrames);
 
 int pageFaults = 0;
 int hits = 0;
 
+//12342156212376321236
 int main(){
-    //char str[100];
-    char str[] = "70120304230321201701";
-    //char choice[5];
-    char choice[] = "FIFO";
+    char str[100];
+    char choice[5];
     int size = 0;
-    //printf("Enter a reference string with no spaces or commas.\n");
-    //fgets(str, sizeof(str), stdin);
 
-    //printf("\nEnter the amount of frames:\n");
-    //scanf("%d", &size);
-    size = 3;
-    //getchar();
-    //printf("\nEnter the page replacement algorithm you would like to use (FIFO, LRU, OPT):\n");
-    //fgets(choice, sizeof(choice), stdin);
+    printf("Enter a reference string with no spaces or commas.\n");
+    fgets(str, sizeof(str), stdin);
 
-    printf("\n");
-    printf("Reference String: %s\n", str);
-    printf("Frame Size: %d\n", size);
-    printf("Page Replacement Algorithm: %s\n", choice);
-    printf("\n");
+    printf("\nEnter the amount of frames:\n");
+    scanf("%d", &size);
 
-    LRU(str,size);
+    getchar();
+    printf("\nEnter the page replacement algorithm you would like to use (FIFO, LRU, OPT):\n");
+    fgets(choice, sizeof(choice), stdin);
+
+    if (strcmp(choice, "LRU")|| strcmp(choice, "lru") == 0) {
+        LRU(str, size);
+    }
+    else if (strcmp(choice, "OPT") || strcmp(choice, "opt") == 0) {
+        OPT(str, size);
+    }
+    else if (strcmp(choice, "FIFO") || strcmp(choice, "fifo") == 0) {
+        FIFO(str, size);
+    }
+    else {
+        printf("Invalid choice\n");
+    }
+
     printf("Page faults: %d\n", pageFaults);
     printf("Hits: %d\n", hits);
     return 0;
@@ -47,6 +54,73 @@ bool isHit(char page, char * frames[], int frameCount){
     return false;
 }
 
+void OPT(char *refString, int maxFrames) {
+    char *memoryPtr[maxFrames];
+    for (int i = 0; i < maxFrames; i++) {
+        memoryPtr[i] = NULL;
+    }
+
+    int hitDetection[maxFrames];
+    for (int i = 0; i < maxFrames; i++) {
+        hitDetection[i] = 0;
+    }
+
+    for (int i = 0; i < strlen(refString); i++) {
+        printf("%c ", refString[i]);
+
+        // Check if page is already in memory
+        int found = 0;
+        for (int j = 0; j < maxFrames; j++) {
+            if (memoryPtr[j] != NULL && *memoryPtr[j] == refString[i]) {
+                found = 1;
+                hits++;
+                break;
+            }
+        }
+
+        if (!found) {
+            // Page fault
+            pageFaults++;
+
+            // Find the optimal page to replace
+            int OPTindex = -1;
+            int farthest = i + 1;
+
+            for (int j = 0; j < maxFrames; j++) {
+                if (memoryPtr[j] == NULL) {
+                    OPTindex = j;
+                    break;
+                }
+
+                int k;
+                for (k = i + 1; k < strlen(refString); k++) {
+                    if (*memoryPtr[j] == refString[k]) {
+                        if (k > farthest) {
+                            farthest = k;
+                            OPTindex = j;
+                        }
+                        break;
+                    }
+                }
+
+                if (k == strlen(refString)) {
+                    OPTindex = j;
+                    break;
+                }
+            }
+
+            memoryPtr[OPTindex] = &refString[i];
+        }
+
+        for (int c = 0; c < maxFrames; c++) {
+            if (memoryPtr[c] != NULL) {
+                printf("%c", *memoryPtr[c]);
+            }
+        }
+        printf("\n");
+    }
+}
+
 void LRU(char * refString, int maxFrames){
 
     char * memoryPtr[maxFrames];
@@ -54,14 +128,15 @@ void LRU(char * refString, int maxFrames){
         memoryPtr[i] = NULL;
     }
 
-    char * hitDetection[maxFrames];                       // Array to store hit bits.
+    char * hitDetection[maxFrames];                                     // Array to store hit bits.
     for (int i = 0; i < maxFrames; i++) {
         hitDetection[i] = 0;
     }
 
-    int time = 0;                                         // Simulate time to get the LRU index.
+    int time = 0;                                                       // Simulate time to get the LRU index.
 
     for (int i = 0; i < strlen(refString); ++i) {
+        printf("%c ", refString[i]);
         time++;
         char currentPage = refString[i];                                // Get current page ref.
 
@@ -88,8 +163,8 @@ void LRU(char * refString, int maxFrames){
             hits++;
         }
 
-        char result[maxFrames];                                     // Temporary array to be returned.
-        for (int c = 0; c < maxFrames; c++) {                       // Loop through frames and return their result.
+        char result[maxFrames];                                         // Temporary array to be returned.
+        for (int c = 0; c < maxFrames; c++) {                           // Loop through frames and return their result.
             if (memoryPtr[c] != NULL ) {
                 result[c] = *memoryPtr[c];
                 printf("%c",result[c]);
@@ -104,6 +179,7 @@ void LRU(char * refString, int maxFrames){
 
 
 void FIFO(char * refString, int maxFrames){
+
     char * memoryPtr[maxFrames];
 
     // Initialize memory frames to NULL.
@@ -114,6 +190,7 @@ void FIFO(char * refString, int maxFrames){
     // Loop through reference string and check if a hit/page faults occurs.
     // Compute the new string that fits into the specified frame amount.
     for (int i=0; i < strlen(refString);++i){
+        printf("%c ", refString[i]);
         if(isHit(refString[i], memoryPtr, maxFrames) == false){     // On page fault.
             memoryPtr[counter] = &refString[i];
             counter++;
